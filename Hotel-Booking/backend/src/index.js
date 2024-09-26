17497093ed8f75b33eb00ccf4bdb72229a07c1bd
Naresh-official <naresh01.official@gmail.com>
2024-09-26 17:47:12 +0530

    install vite

--- Hotel-Booking/backend/src/index.js
+++ Hotel-Booking/backend/src/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+import dbConnect from "./utils/dbConnect.js";
+
+dotenv.config({
+    path: "./.env",
+});
 
 const app = express();
 
@@ -6,6 +12,12 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
-});
+dbConnect()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Example app listening on port 3000!");
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB : ", error);
+    });
---
+++ Hotel-Booking/backend/src/utils/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export default async function dbConnect() {
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB : ", db.connection.host);
+        return db;
+    } catch (error) {
+        console.error("Error connecting to MongoDB : ", error);
+        process.exit(1);
+    }
+}
