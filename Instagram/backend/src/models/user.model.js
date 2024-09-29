6065c61297230c3141d142591ce5ed6b528d044e
Naresh-official <naresh01.official@gmail.com>
2024-09-29 15:41:24 +0530

    create user model

---
+++ Instagram/backend/src/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImg: {
+        type: String,
+        default:
+            "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541",
+    },
+    bio: {
+        type: String,
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+    },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    post: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    saved: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET);
+};
+
+userSchema.methods.comparePassword = async function (password) {
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
